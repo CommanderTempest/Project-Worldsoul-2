7298cb831fff2f3d5cab8a264210473e82c93205
CommanderTempest <krukovtempest@gmail.com>
2022-03-08 14:06:01 -0600

    Button Movement
    The buttons can now be dragged around the screen.

--- src/client/hotbarUI.client.tsx
+++ src/client/hotbarUI.client.tsx
@@ -17,6 +17,11 @@ let actionIsMoving = false;
 let actionDebounce = false;
 let action;
 
+const mouseToButtonOffsetX = 56; // 112/2 -> half of the UI button's size
+const mouseToButtonOffsetY = 20; // an offset to put the mouse in the middle of the button
+const frameXOffset = 200;        // The frame's X position
+const frameYOffset = 474;        // The frame's Y Position
+
 class HotbarUI extends Roact.Component<UIProperties, UIState>
 {
   render()
@@ -31,36 +36,50 @@ class HotbarUI extends Roact.Component<UIProperties, UIState>
 
           <textbutton 
             Text = {"Button1"}
-            Position = {new UDim2(0,112,0,0)}
+            Position = {new UDim2(0,0,0,0)}
             Size = {new UDim2(0, 112, 0, 50)}
           />
           <textbutton 
             Text = {"Button2"}
-            Event = {{}}
+            Position = {new UDim2(0,112,0,0)}
             Size = {new UDim2(0, 112, 0, 50)}
           />
-
-          <imagelabel
-            Size = {new UDim2(0, 44, 0, 50)}
-            // Insert image into here too
-          />
+          
+          
         </frame>
       </screengui>
     )
   }
 }
 
+/* stored here cuz it caused a problem
+<imagelabel
+            Size = {new UDim2(0, 44, 0, 50)}
+            Position = {new UDim2(0, 900, 0, 0)}
+            // Insert image into here too
+          />
+*/
+
 // the action is currently moving
 
-function actionMoving(action: TextButton)
+function actionMoving(x: number, y: number, action: TextButton)
 {
+  print(action);
+  print(y);
   actionIsMoving = true;
+  let mouse;
   if (actionDebounce === false)
   {
     actionDebounce = true;
 
     while (actionIsMoving)
     {
+      mouse = Players.LocalPlayer.GetMouse();
+      wait(0.01);
+      action.Position =
+        new UDim2(
+          0, ((Players.LocalPlayer.GetMouse().X - frameXOffset) - mouseToButtonOffsetX), 
+          0, ((Players.LocalPlayer.GetMouse().Y - frameYOffset) - mouseToButtonOffsetY));
       // move button based on mouse position and calculations
     } // end while
     actionDebounce = false;
@@ -68,18 +87,18 @@ function actionMoving(action: TextButton)
 
 } // end actionMoving
 
-function stopActionMoving(action: TextButton)
+function stopActionMoving()
 {
   print("Stopping movement!");
   actionIsMoving = false; // stop the while loop in the other function...? Wait does this stop it.
 }
 
 let myHandle = Roact.mount(<HotbarUI/>, playerGui, "HotbarUI");
-let myButtonArray = playerGui.FindFirstChild("HotbarUI")?.GetChildren() as Array<TextButton>;
+let myFrame = playerGui.FindFirstChild("HotbarUI")?.FindFirstChild("1") as Frame;
+let myButtonArray = myFrame.GetChildren() as Array<TextButton>;
 
 myButtonArray?.forEach((element) => {
-  print("Connection Made!");
-  element.MouseButton1Down.Connect(() => actionMoving);
-  element.MouseButton1Up.Connect(() => stopActionMoving);
+  element.MouseButton1Down.Connect((x, y) => actionMoving(x, y, element));
+  element.MouseButton1Up.Connect(() => stopActionMoving());
 })
 
