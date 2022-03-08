6b12c80c5916802809bae4ca3ece94e4d6afb97b
CommanderTempest <krukovtempest@gmail.com>
2022-03-08 15:27:01 -0600

    SWAP WORKS
    I CRACKED THE CODE! Swapping now works.

--- src/client/hotbarUI.client.tsx
+++ src/client/hotbarUI.client.tsx
@@ -10,12 +10,13 @@ interface UIProperties
 
 interface UIState
 {
-  positions: Array<UDim2>;
+
 }
 
 let actionIsMoving = false;
 let actionDebounce = false;
-let prevActionPos;
+let prevAction: TextButton;
+let orgPos: UDim2;
 
 const mouseToButtonOffsetX = 56; // 112/2 -> half of the UI button's size
 const mouseToButtonOffsetY = 20; // an offset to put the mouse in the middle of the button
@@ -29,7 +30,7 @@ class HotbarUI extends Roact.Component<UIProperties, UIState>
     return (
       <screengui ResetOnSpawn={false}>
         <frame 
-          Size={new UDim2(0, 900, 0, 50)} 
+          Size={new UDim2(0, 856, 0, 50)} 
           Position={new UDim2(0, 200, 0, 474)}         // Center bottom
           BackgroundColor3={new Color3(138, 138, 138)} // A lightish gray
           >
@@ -44,23 +45,11 @@ class HotbarUI extends Roact.Component<UIProperties, UIState>
             Position = {new UDim2(0,112,0,0)}
             Size = {new UDim2(0, 112, 0, 50)}
           />
-          
-          
         </frame>
       </screengui>
     )
-  }
+  } // end render
 }
-
-/* stored here cuz it caused a problem
-<imagelabel
-            Size = {new UDim2(0, 44, 0, 50)}
-            Position = {new UDim2(0, 900, 0, 0)}
-            // Insert image into here too
-          />
-*/
-
-// the action is currently moving
 
 /********************************************
   actionMoving moves the textButton's around
@@ -69,6 +58,7 @@ class HotbarUI extends Roact.Component<UIProperties, UIState>
 
 function actionMoving(x: number, y: number, action: TextButton)
 {
+  orgPos = action.Position;
   let mouse; 
   actionIsMoving = true;
   
@@ -98,34 +88,55 @@ function actionMoving(x: number, y: number, action: TextButton)
 function stopActionMoving(x: number, y: number, action: TextButton)
 {
   let xOffset = x - frameXOffset;
+  let temp: UDim2;
   actionIsMoving = false; // stop the while loop in actionMoving()
 
   // update the button's position to a prespecified spot on the hotbar
-  if (xOffset >= 0 && xOffset < 112)
+  wait(0.1);
+  if (xOffset >= 0 && xOffset < 112)       
   {
-    action.Position = new UDim2(0,0,0,0);
-  }
-  else if(xOffset >= 112 && xOffset < 224)
-  {
-    action.Position = new UDim2(0,112,0,0);
-  }
+    if (checkForMatches(new UDim(0,0)) === true)
+    {
+      print("test1");
+      print(prevAction.Position);
+      print(orgPos);
+      prevAction.Position = orgPos;
+    } // end if
+    action.Position = new UDim2(0,0,0,0);    
+  } // end if
+  else if(xOffset >= 112 && xOffset < 224) {action.Position = new UDim2(0,112,0,0);}
   else if(xOffset >= 224 && xOffset < 336) {action.Position = new UDim2(0,224,0,0);}
-
-  /*
-    I wonder if there's a conflict here between manually updating
-    and Roact, since i'm not using Roact.update
-  */
 
   // debounce reset here, to prevent user from
   // moving button again while it's still being moved.
   actionDebounce = false;
 } // end stopActionMoving
 
+function checkForMatches(xToSwapTo: UDim): boolean
+{
+  // I need this variable, because I can't return inside
+  // of the forEach loop I guess *shrug*
+  let foundToBeTrue: boolean = false;
+
+  myButtonArray.forEach((element) => {
+    if (element.Position.X === xToSwapTo)
+    {
+      prevAction = element;
+      foundToBeTrue = true;
+    }
+  })
+  return foundToBeTrue;
+  return false;
+}
+
 let myHandle = Roact.mount(<HotbarUI/>, playerGui, "HotbarUI");
 let myFrame = playerGui.FindFirstChild("HotbarUI")?.FindFirstChild("1") as Frame;
 let myButtonArray = myFrame.GetChildren() as Array<TextButton>;
 
+let i = 0;
 myButtonArray?.forEach((element) => {
+  element.Name = tostring(i);
+  i++;
   element.MouseButton1Down.Connect((x, y) => actionMoving(x, y, element));
   element.MouseButton1Up.Connect((x,y) => stopActionMoving(x, y, element));
 })
